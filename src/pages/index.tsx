c598d57bad338f481fc2a22db61e86991714992b
leoTomio <leo__tomio@hotmail.com>
2023-03-09 09:23:25 -0300

    Criando Componentes (button + input + textArea), scss e a tela de login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,40 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../../styles/home.module.scss'
 
+import logoImg from '../../public/logo.svg'
+
+import { Input } from '../components/ui/Input'
+import { Button } from '../components/ui/Button'
 export default function Home() {
   return (
-    <div>
-      <h1>Projeto Pizzaria</h1>
-    </div>
+    <>
+      <Head>
+        <title>Pizzaria - Faça seu login</title>
+      </Head>
+      <div className={styles.containerCenter}>
+        <Image src={logoImg} alt="Pizzaria" />
+        <div className={styles.login}>
+          <form>
+            <Input
+              placeholder='Digite seu email'
+              type="email" />
+
+            <Input
+              placeholder='Senha'
+              type="password" />
+
+            <Button
+              type="submit"
+              loading={false}
+            >
+              Acessar
+            </Button>
+
+          </form>
+          <a className={styles.text}>Não possui uma conta? <b>Cadastre-se</b></a>
+        </div>
+      </div>
+    </>
   )
 }
