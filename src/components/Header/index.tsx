fd78cf546f74dbd9f58a3c89657273a534e9a6dc
leoTomio <leo__tomio@hotmail.com>
2023-03-20 11:47:34 -0300

    0.0.6 -> Criando header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,41 @@
+import { useContext } from 'react'
+import styles from './styles.module.scss'
+import Link from 'next/link'
+
+import { FiLogOut } from 'react-icons/fi'
+import { AuthContext } from '../../contexts/AuthContext'
+
+export function Header() {
+
+    const { user, signOut } = useContext(AuthContext)
+
+    return (
+        <header className={styles.headerContainer}>
+            <div className={styles.headerContent}>
+
+                <Link href="/dashboard" legacyBehavior>
+                    <img src="/logo.svg" width={190} height={60} />
+                </Link>
+
+                <div className={styles.userDiv}><h2><b>Usuario:</b> {user?.name}</h2></div>
+
+                <nav className={styles.menuNav}>
+
+                    <Link href="/category" legacyBehavior>
+                        <a>Categoria</a>
+                    </Link>
+
+                    <Link href="/product" legacyBehavior>
+                        <a>Cardapio</a>
+                    </Link>
+
+                    <button onClick={signOut}>
+                        <FiLogOut color='#fff' size={24} />
+                    </button>
+                </nav>
+
+            </div>
+        </header>
+    )
+
+}
